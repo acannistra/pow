aa21d52fc911d52c22bd7bba4350ffd6a23a6258
Tony Cannistra <tony.cannistra@gmail.com>
2019-12-23 10:59:22 -0500

    updates to db config

--- db/config.py
+++ db/config.py
@@ -9,20 +9,45 @@ def cli(**kwargs):
 
 @cli.command()
 @click.argument('name', default='pow.db')
+@click.option("--file", help="JSON File containing stations to load.")
 def init(**kwargs):
     print("Creating sqlite3 database [{}]...".format(kwargs['name']))
     conn = sqlite3.connect(kwargs['name'])
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS stations
-                 (name text, description text, url text, lon float, lat float,
+                 (name text, description text, elevation int, data_url text, snow_field text, lon float, lat float,
                  CONSTRAINT name PRIMARY KEY (name));''')
+    c.execute('''CREATE TABLE IF NOT EXISTS params
+                 (name text, value text);''')
+
+    if kwargs['file']:
+        stations = json.load(open(kwargs['file']))
+        for station in stations:
+            load_command = "INSERT INTO stations VALUES (?, ?, ?, ?, ?, ?, ?)"
+            print("Loading Station {}".format(station.get('name')))
+            c.execute(load_command, (
+                station.get('name'),
+                station.get('desc', None),
+                station.get('elevation', -1),
+                station.get('url', None),
+                station.get('snow_field', 'snow_depth'),
+                station.get('lon'),
+                station.get('lat')
+            ))
+
+        c.executemany("INSERT INTO params VALUES (?, ?)", [
+            ("station", stations[0].get('name')),
+            ("snowfall_threshold", 6),
+            ("accumulation_period", 24)
+        ])
     conn.commit()
     conn.close()
 
 @cli.command()
 @click.argument('db')
 @click.option('--name', '-n', help="Station Name")
-@click.option('--url', help="URL of wx data")
+@click.option('--data_url', '--url', help="URL of wx data")
+@click.option("--snow_field", '--field', help="Field name in data CSV containing hourly snowfall amounts.")
 @click.option('--description', '--desc', help="Station Description")
 @click.option('--location', help="(lat,lon)")
 def addStation(**kwargs):
