4065a8e78103a3bb88d8ae176a9f4b8f332cefe3
Tony Cannistra <tony.cannistra@gmail.com>
2019-12-23 10:59:41 -0500

    server stuff

--- server/pow-daemon.py
+++ server/pow-daemon.py
@@ -1,27 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
-THRESHOLD = None
-LOCATION = None
-HOURS = None
+DB = '../pow.db'
 
+@app.route('/stations', methods=['GET'])
+def stations():
+    conn = sqlite3.connect(DB)
+    c = conn.cursor()
+    q = c.execute('SELECT * FROM {};'.format('stations'))
+    r = [dict((q.description[i][0], value) \
+               for i, value in enumerate(row)) for row in q.fetchall()]
+    return jsonify(r)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    THRESHOLD = None
-    LOCATION = None
-    HOURS = None
+
     if request.method == 'POST':
-        print request.form
-        LOCATION = request.form.get('location')
-        THRESHOLD = request.form.get('snowfall_threshold')
-        HOURS = request.form.get('accumulation_period')
-    return render_template("index.html", **{
-        'location': LOCATION,
-        'snowfall_threshold': THRESHOLD,
-        'accumulation_period' : HOURS
-    })
+        print(request)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
