e66d013cdebf78c41c85d185f29f2509ab583096
Tony Cannistra <tony.cannistra@gmail.com>
2020-11-14 14:35:31 -0800

    new daemon scaffold

--- daemon/powalert.py
+++ daemon/powalert.py
@@ -1,27 +1,41 @@
-import sqlite3
 import requests
+import os
 import click
 import sys
+import json
+import logging
 
 sys.path.append('..')
 
-def get_params_db(db, table='params'):
-    """
-        Get POW alerting parameters from database.
-    """
-    conn = sqlite3.connect(db)
-    c = conn.cursor()
-    q = c.execute('SELECT * FROM {};'.format(table))
-    r = [dict((q.description[i][0], value) \
-               for i, value in enumerate(row)) for row in q.fetchall()]
-    return(r)
+POW_API_URL = os.environ.get("POW_API_URL", "https://81s7s0fg1e.execute-api.us-west-2.amazonaws.com/dev/pow")
+POW_GPIO_PIN = os.environ.get("POW_GPIO_PIN", 21)
+
+def get_status(station, threshold, period):
+    params = {
+        'station': station,
+        'threshold':  threshold,
+        'period': period
+    }
+    logging.info(f"Getting pow status for {station} (threshold: {threshold}, period: {period})...")
+    r = requests.get(POW_API_URL, params=params)
+    logging.info(f"API Response: {r.json()}")
+    return r.json()['is_pow']
+
 
 @click.command()
-@click.argument('DB')
-def daemon(db):
-    print("Loading parameters from db [{}]".format(db))
-    print(get_params_db(db))
-    pass
+@click.argument('config')
+@click.argument("gpio", default=POW_GPIO_PIN)
+def daemon(config, gpio):
+    logging.basicConfig(level=logging.INFO)
+    params = json.load(open(config))
+
+    while True:
+        status = get_status(params['station'], params['threshold'], params['period'])
+
+        sleep(params['poll_interval_s'])
+
+
+
 
 
 if __name__ == '__main__':
